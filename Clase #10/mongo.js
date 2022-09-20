6cb6e1a826a4fc3a03cab132c29bf57768aa1a3d
Brian Lara <brian.lara@outlook.com.ar>
2022-09-20 18:02:15 -0300

    Mongo Driver Node

---
+++ Clase #10/mongo.js
@@ -0,0 +1,77 @@
+import { MongoClient, ObjectId } from "mongodb"
+
+const client = new MongoClient("mongodb://127.0.0.1:27017")
+
+client.connect()
+    .then(async function () {
+        console.log("Connected to MongoDB")
+        const db = client.db("DB_T") //  use DB_T
+        console.log(db.databaseName)
+
+        const Alumnos = db.collection("Alumnos") // db.Alumnos
+
+        const AlumnosList = await Alumnos.find().toArray()
+
+        console.log("Lista de alumnos")
+        console.table(AlumnosList)
+
+        const AlumnosFiltro = await Alumnos.find({ nota: 7 }).toArray()
+
+        console.log("Lista de alumnos con nota 7")
+        console.table(AlumnosFiltro)
+
+        const AlumnosOrdenados = await Alumnos.find().sort({ name: -1 }).toArray()
+
+        console.log("Lista de alumnos ordenados por nombre")
+        console.table(AlumnosOrdenados)
+
+        const Alumno = await Alumnos.findOne({ _id: new ObjectId("6324e28e46ead9daae28093c") })
+
+        console.log("Alumno con id 6324e28e46ead9daae28093c")
+        console.table(Alumno)
+
+        // Insertar un alumno
+
+        const AlumnoNuevo = {
+            name: "Juan",
+            nota: 8
+        }
+
+        // side effect por que es una funcion impura
+        /*
+            for (let i = 0; i < 100; i++) {
+                const result = await Alumnos.insertOne({ ...AlumnoNuevo })
+                console.log("Alumno insertado")
+                console.log(AlumnoNuevo)
+                console.log(result)
+    
+            }
+        */
+
+
+        // eliminar un alumno
+
+        //const result = await Alumnos.deleteOne({ _id: new ObjectId("632a259debe5723351ba48ff") })
+        /*
+        const result = await Alumnos.deleteMany({ name: "Juan" })
+        console.log("Alumno eliminado")
+        console.log(result)
+        */
+
+        // actualizar un alumno
+        const result = await Alumnos.updateOne({
+            _id: new ObjectId("6324e28e46ead9daae28093c")
+        }, {
+            $unset: { email: '' }
+        })
+
+        console.log("Alumno actualizado")
+        console.log(result)
+
+
+
+
+    })
+    .catch(function (err) {
+        console.log(err)
+    })
